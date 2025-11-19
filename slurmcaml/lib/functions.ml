f516ba6c626e4c32bb86e8e7bcf6fe1fc849214c
Mukund <mg2476@cornell.edu>
2025-11-19 00:25:28 -0500

    implement functions on integer and float matrices

--- slurmcaml/lib/functions.ml
+++ slurmcaml/lib/functions.ml
@@ -12,8 +12,99 @@ end
 module IntegerMatrixOperations : MatrixOperations with type t = int = struct
   type t = int
 
-  let add mat1 mat2 = mat1
-  let subtract mat1 mat2 = mat1
-  let multiply mat1 mat2 = mat1
-  let scale k mat1 = mat1
+  (* HN can't send the whole matrix, has to only send the specific rows to
+     operate on*)
+  let add_rows row1 row2 =
+    let length = Array.length row1 in
+    let elt i = row1.(i) + row2.(i) in
+    Array.init length elt
+
+  let subtract_rows row1 row2 =
+    let length = Array.length row1 in
+    let elt i = row1.(i) - row2.(i) in
+    Array.init length elt
+
+  let add mat1 mat2 =
+    let length = Array.length mat1 in
+    let calc i = add_rows mat1.(i) mat2.(i) in
+    Array.init length calc
+
+  let subtract mat1 mat2 =
+    let length = Array.length mat1 in
+    let calc i = subtract_rows mat1.(i) mat2.(i) in
+    Array.init length calc
+
+  let extract_column j mat =
+    let length = Array.length mat in
+    let elt i = mat.(i).(j) in
+    Array.init length elt
+
+  let dot_product row1 row2 =
+    let sum = ref 0 in
+    for i = 0 to Array.length row1 - 1 do
+      sum := !sum + (row1.(i) * row2.(i))
+    done;
+    !sum
+
+  let calc_row i mat1 mat2 =
+    let elt j = dot_product mat1.(i) (extract_column j mat2) in
+    Array.init (Array.length mat2.(0)) elt
+
+  let multiply mat1 mat2 =
+    Array.init (Array.length mat1) (fun i -> calc_row i mat1 mat2)
+
+  let scale_row k row = Array.init (Array.length row) (fun i -> k * row.(i))
+
+  let scale k mat1 =
+    Array.init (Array.length mat1) (fun i -> scale_row k mat1.(i))
+end
+
+module FloatMatrixOperations : MatrixOperations with type t = float = struct
+  type t = float
+
+  (* HN can't send the whole matrix, has to only send the specific rows to
+     operate on*)
+  let add_rows row1 row2 =
+    let length = Array.length row1 in
+    let elt i = row1.(i) +. row2.(i) in
+    Array.init length elt
+
+  let subtract_rows row1 row2 =
+    let length = Array.length row1 in
+    let elt i = row1.(i) -. row2.(i) in
+    Array.init length elt
+
+  let add mat1 mat2 =
+    let length = Array.length mat1 in
+    let calc i = add_rows mat1.(i) mat2.(i) in
+    Array.init length calc
+
+  let subtract mat1 mat2 =
+    let length = Array.length mat1 in
+    let calc i = subtract_rows mat1.(i) mat2.(i) in
+    Array.init length calc
+
+  let extract_column j mat =
+    let length = Array.length mat in
+    let elt i = mat.(i).(j) in
+    Array.init length elt
+
+  let dot_product row1 row2 =
+    let sum = ref 0.0 in
+    for i = 0 to Array.length row1 - 1 do
+      sum := !sum +. (row1.(i) *. row2.(i))
+    done;
+    !sum
+
+  let calc_row i mat1 mat2 =
+    let elt j = dot_product mat1.(i) (extract_column j mat2) in
+    Array.init (Array.length mat2.(0)) elt
+
+  let multiply mat1 mat2 =
+    Array.init (Array.length mat1) (fun i -> calc_row i mat1 mat2)
+
+  let scale_row k row = Array.init (Array.length row) (fun i -> k *. row.(i))
+
+  let scale k mat1 =
+    Array.init (Array.length mat1) (fun i -> scale_row k mat1.(i))
 end
