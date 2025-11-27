c232a4067119bd5db6ef0626baba5401da567d89
Naijei Jiang <naijeiwow@gmail.com>
2025-11-27 12:58:40 -0500

    implement matrix compilation

--- bin/server.ml
+++ bin/server.ml
@@ -1,9 +1,20 @@
 open Slurmcaml.Matrixutils
 
 let users = Hashtbl.create 10
-let head_props = ref None
+let head_props = ref Lwt_io.stdout
 let jobs = Queue.create ()
+
+(* ignoring this for now*)
 let job_table = Hashtbl.create 10
+
+type currentJobType =
+  | IntJobType
+  | FloatJobType
+
+(* this is tracking the job type, the worker assignments, the result matrix, and
+   the number of workers who have finished their work*)
+let assignment_table =
+  ref (IntJobType, Hashtbl.create 10, IntMatrix [| [| 0 |] |], 0)
 
 let sock_addr_to_string (add : Unix.sockaddr) =
   match add with
@@ -47,7 +58,7 @@ let process_job in_channel =
       let%lwt mat1 = read_int_matrix_input in_channel in
       let%lwt () = Lwt_io.printf "mat1: %d rows\n%!" (Array.length mat1) in
       let%lwt mat2 = read_int_matrix_input in_channel in
-      let%lwt () = Lwt_io.printf "mat1: %d rows\n%!" (Array.length mat2) in
+      let%lwt () = Lwt_io.printf "mat2: %d rows\n%!" (Array.length mat2) in
       Lwt.return (IntJob { aint = mat1; bint = mat2; opi = op })
   | "float" ->
       let%lwt mat1 = read_float_matrix_input in_channel in
@@ -68,6 +79,66 @@ let split_matrix mat num =
       let start = (i * base) + min i extra in
       Array.sub mat start size)
 
+let determine_assignments job =
+  match job with
+  | IntJob { aint; bint; opi } ->
+      let mat = aint in
+      let workers = List.of_seq (Hashtbl.to_seq users) in
+      let num = List.length workers in
+      let n = Array.length mat in
+      let base = n / num in
+      let extra = n mod num in
+      let worker_assignments = Hashtbl.create 10 in
+      List.iteri
+        (fun i (sock, (username, _, _, _)) ->
+          let size = base + if i < extra then 1 else 0 in
+          let start = (i * base) + min i extra in
+          Hashtbl.replace worker_assignments sock (start, start + size))
+        workers;
+      ( IntJobType,
+        worker_assignments,
+        construct_int_matrix (Array.length aint) (Array.length aint.(0)),
+        0 )
+  | FloatJob { afloat; bfloat; opf } ->
+      let mat = afloat in
+      let workers = List.of_seq (Hashtbl.to_seq users) in
+      let num = List.length workers in
+      let n = Array.length mat in
+      let base = n / num in
+      let extra = n mod num in
+      let worker_assignments = Hashtbl.create 10 in
+      List.iteri
+        (fun i (sock, (username, _, _, _)) ->
+          let size = base + if i < extra then 1 else 0 in
+          let start = (i * base) + min i extra in
+          Hashtbl.replace worker_assignments sock (start, start + size))
+        workers;
+      ( FloatJobType,
+        worker_assignments,
+        construct_float_matrix (Array.length afloat) (Array.length afloat.(0)),
+        0 )
+
+(*Portion is tuple, first row - last row(Non inclusive), Res is result. Need
+  function that fills in matrix given the result matrix and portion, Updates
+  assignment_table*)
+let fill_matrix_int (res : int array array) ((first_row, last_row) : 'a * 'b)
+    (old_array : int array array) =
+  print_endline "entering fill matrix";
+  print_int first_row;
+  print_int last_row;
+  Array.iteri
+    (fun num_row matrix_row -> old_array.(num_row + first_row) <- matrix_row)
+    res
+
+let fill_matrix_float (res : float array array)
+    ((first_row, last_row) : 'a * 'b) (old_array : float array array) =
+  Array.iteri
+    (fun num_row matrix_row -> old_array.(num_row + first_row) <- matrix_row)
+    res
+
+(*[[1,2,3][4,5,6]] *)
+(*123
+  456*)
 let split_int_job numworkers job =
   let { aint; bint; opi } = job in
   let a_chunks = split_matrix aint numworkers in
@@ -112,8 +183,6 @@ let dispatch_job split =
 
 let client_handler client_socket_address (client_in, client_out) =
   let key = sock_addr_to_string client_socket_address in
-
-  (*client should automatically send their node type as the first msg*)
   let%lwt node_type_opt = Lwt_io.read_line_opt client_in in
   match node_type_opt with
   | None ->
@@ -126,7 +195,7 @@ let client_handler client_socket_address (client_in, client_out) =
       | "CLIENT" ->
           let%lwt () = Lwt_io.printlf "CLIENT node connected: %s" key in
           let%lwt () =
-            head_props := Some (key, client_in, client_out);
+            head_props := client_out;
             Lwt.return_unit
           in
           let rec handle_jobs () =
@@ -146,18 +215,20 @@ let client_handler client_socket_address (client_in, client_out) =
                   in
                   let%lwt () =
                     Lwt_io.fprintl client_out
-                      "There are no worker nodes available, please try again \
+                      "There are no worker nodes available, please try again  \
                        later"
                   in
                   handle_jobs ()
                 else
+                  let () = assignment_table := determine_assignments job in
                   let split =
                     match job with
                     | IntJob i -> split_int_job (Hashtbl.length users) i
                     | FloatJob j -> split_float_job (Hashtbl.length users) j
                   in
                   let%lwt () = dispatch_job split in
-
+                  (* determine assignments and then update the table with jobid,
+                     assignments *)
                   handle_jobs ()
             | Some command ->
                 let%lwt () = Lwt_io.printlf "Received job from %s\n" key in
@@ -174,7 +245,6 @@ let client_handler client_socket_address (client_in, client_out) =
             Lwt.return_unit
           in
           let%lwt () = Lwt_io.printlf "%s (%S) connected." key username in
-
           let rec handle_status () =
             let%lwt status = Lwt_io.read_line_opt client_in in
             match status with
@@ -185,57 +255,37 @@ let client_handler client_socket_address (client_in, client_out) =
                 Hashtbl.remove users key;
                 Lwt.return_unit
             | Some status -> (
-                let%lwt () =
-                  Lwt_io.printlf "%s (%s): Status Update: %S" key username
-                    status
-                in
-
-                (*statuses are formatted as follows: <AVAILABLE> <JOB>
-                  <OUTPUT>*)
-                match parse_status_string status with
-                | None ->
+                match !assignment_table with
+                | IntJobType, tbl, IntMatrix work, counter ->
+                    let%lwt res = read_int_matrix_input client_in in
+                    let portion = Hashtbl.find tbl key in
+                    let () = fill_matrix_int res portion work in
+                    assignment_table :=
+                      (IntJobType, tbl, IntMatrix work, counter + 1);
                     let%lwt () =
-                      Lwt_io.printlf "%s (%s): Malformed status string: %s" key
-                        username status
+                      if counter + 1 = Hashtbl.length users then
+                        print_matrix (IntMatrix work) Lwt_io.stdout
+                      (*!head_props*) else Lwt.return ()
                     in
-                    handle_status ()
-                | Some ("AVAILABLE", job, output) ->
-                    Hashtbl.replace users key
-                      (username, client_in, client_out, true);
-                    Hashtbl.replace job_table job
-                      (true, Some username, Some output);
-                    handle_status ()
-                | _ -> handle_status ())
+                    handle_status () (* print to client*)
+                | FloatJobType, tbl, FloatMatrix work, counter ->
+                    let%lwt res = read_float_matrix_input client_in in
+                    let portion = Hashtbl.find tbl key in
+                    let () = fill_matrix_float res portion work in
+                    assignment_table :=
+                      (FloatJobType, tbl, FloatMatrix work, counter + 1);
+                    let%lwt () =
+                      if counter + 1 = Hashtbl.length users then
+                        print_matrix (FloatMatrix work) Lwt_io.stdout
+                      (*!head_props *) else Lwt.return ()
+                    in
+                    handle_status () (*print to client *)
+                | _ -> Lwt.fail_with "this should not happen :(")
           in
-          let rec send_jobs () =
-            match Queue.is_empty jobs with
-            | true ->
-                let%lwt () = Lwt_unix.sleep 1.0 in
-                send_jobs ()
-            | false -> (
-                match find_available_worker () with
-                | None ->
-                    let%lwt () = Lwt_unix.sleep 1.0 in
-                    send_jobs ()
-                | Some (worker_key, worker_name, worker_in, worker_out) ->
-                    let job = Queue.pop jobs in
-                    let%lwt () =
-                      Lwt_io.printlf "Assigning job %s to worker %s (%s)" job
-                        worker_name worker_key
-                    in
-                    let%lwt () =
-                      Lwt.return
-                        (Hashtbl.replace job_table job
-                           (false, Some worker_name, None))
-                    in
-                    let%lwt () = Lwt_io.write_line worker_out job in
-                    let%lwt () = Lwt_io.flush worker_out in
-                    Hashtbl.replace users worker_key
-                      (worker_name, worker_in, worker_out, false);
-                    send_jobs ())
-          in
-          Lwt.choose [ handle_status (); send_jobs () ])
+          handle_status ())
 
+(*Portion is tuple, first row - last row(Non inclusive), Res is result. Need
+  function that fills in matrix given the result matrix and portion*)
 let run_server ipaddr port =
   let server () =
     let%lwt () = Lwt_io.printlf "Starting Server..." in
@@ -288,3 +338,50 @@ let _ =
    Lwt_list.iter_s (fun line -> Lwt_io.fprintlf client_out "%s" line) (List.rev
    entries) in let%lwt () = Lwt_io.fprintlf client_out "END_OF_JOBS" in
    handle_jobs ()*)
+
+(* let rec send_jobs () = match Queue.is_empty jobs with | true -> let%lwt () =
+   Lwt_unix.sleep 1.0 in send_jobs () | false -> ( match find_available_worker
+   () with | None -> let%lwt () = Lwt_unix.sleep 1.0 in send_jobs () | Some
+   (worker_key, worker_name, worker_in, worker_out) -> let job = Queue.pop jobs
+   in let%lwt () = Lwt_io.printlf "Assigning job %s to worker %s (%s)" job
+   worker_name worker_key in let%lwt () = Lwt.return (Hashtbl.replace job_table
+   job (false, Some worker_name, None)) in let%lwt () = Lwt_io.write_line
+   worker_out job in let%lwt () = Lwt_io.flush worker_out in Hashtbl.replace
+   users worker_key (worker_name, worker_in, worker_out, false); send_jobs ())
+   in Lwt.choose [ handle_status (); send_jobs () ]) *)
+(* (
+let%lwt status = Lwt_io.read_line_opt client_in in
+            match status with
+            | None ->
+                let%lwt () =
+                  Lwt_io.printlf "%s (%s) disconnected." key username
+                in
+                Hashtbl.remove users key;
+                Lwt.return_unit
+            | Some status -> (
+                let%lwt () =
+                  Lwt_io.printlf "%s (%s): Status Update: %S" key username
+                    status
+                in
+                (*statuses are formatted as follows: <AVAILABLE> <JOB>
+                  <OUTPUT>*)
+                match parse_status_string status with
+                | None ->
+                    let%lwt () =
+                      Lwt_io.printlf "%s (%s): Malformed status string: %s" key
+                        username status
+                    in
+                    handle_status ()
+                | Some ("AVAILABLE", job, output) ->
+                    Hashtbl.replace users key
+                      (username, client_in, client_out, true);
+                    Hashtbl.replace job_table job
+                      (true, Some username, Some output);
+
+                    (* read from worker — last worker will do the compilation
+                       work*)
+
+                    (* start, end, *)
+                    handle_status ()
+                | _ -> handle_status ()) 
+)*)
