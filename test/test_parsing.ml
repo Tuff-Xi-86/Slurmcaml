c516226b1a7a3d750c70adc8d86bd13af98a7ff9
Geneustace Wicaksono <gjw62@cornell.edu>
2025-12-05 00:46:51 -0500

    file path tests

--- test/test_parsing.ml
+++ test/test_parsing.ml
@@ -53,6 +53,43 @@ let tests =
            let res = process_op rest_string in
            assert_equal "transpose" res;
            assert_equal "" !rest_string );
+         ( "parse_path" >:: fun _ ->
+           let rest_string = ref "mat1.csv " in
+           let res = parse_path rest_string in
+           assert_equal "mat1.csv" res;
+           assert_equal "" !rest_string );
+         ( "parse_path invalid" >:: fun _ ->
+           let rest_string = ref "mat67.csv " in
+           assert_raises
+             (InvalidMatrixArgument
+                "File path mat67.csv does not exist or is not a .csv file")
+             (fun () -> parse_path rest_string) );
+         ( "read_int_matrix_from_file valid" >:: fun _ ->
+           let path = "mat1.csv" in
+           let mat = read_int_matrix_from_file path in
+           let expected_mat = IntMatrix [| [| 1; 2 |]; [| 3; 4 |] |] in
+           assert_equal expected_mat mat );
+         ( "read_int_matrix_from_file invalid" >:: fun _ ->
+           let path = "mat1unreadable.csv" in
+           assert_raises MatrixNotReadable (fun () ->
+               read_int_matrix_from_file path) );
+         ( "read_int_matrix_from_file invalid2" >:: fun _ ->
+           let path = "mat1unreadable2.csv" in
+           assert_raises MatrixNotReadable (fun () ->
+               read_int_matrix_from_file path) );
+         ( "read_float_matrix_from_file valid" >:: fun _ ->
+           let path = "mat1.csv" in
+           let mat = read_float_matrix_from_file path in
+           let expected_mat = FloatMatrix [| [| 1.; 2. |]; [| 3.; 4. |] |] in
+           assert_equal expected_mat mat );
+         ( "read_float_matrix_from_file invalid" >:: fun _ ->
+           let path = "mat1unreadable.csv" in
+           assert_raises MatrixNotReadable (fun () ->
+               read_float_matrix_from_file path) );
+         ( "read_float_matrix_from_file invalid2" >:: fun _ ->
+           let path = "mat1unreadable2.csv" in
+           assert_raises MatrixNotReadable (fun () ->
+               read_float_matrix_from_file path) );
        ]
 
 let _ = run_test_tt_main tests
